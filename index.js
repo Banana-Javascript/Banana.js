48297ab09a5ef06c04f28802d8bcd726476eb41f
a <50461810+sillyangel@users.noreply.github.com>
2022-04-29 22:44:35 +0000

    heheah

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function isPass(string) {
-  return string === "NodejsCool"
+  console.log("Bananajs is Starting up.")
+
+  // Vars and Stuff
+
+  var verison = "v1.1.0"
+  var CODEHOLDER = "sillyangel"
+
+
+    console.log(`Banana.js, ${verison}, made by ${CODEHOLDER}`)
+
+
+function Banana(string) {
+  if (string === "NodejsCool") {
+    console.log("Nodejs is Cool")
+  }
+  if(string === "help") {
+    console.log("you mean Bhelp")
+  }
 }
 
-module.exports = isPass
-
+module.export = Banana
