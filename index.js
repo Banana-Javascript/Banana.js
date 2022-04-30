68b196edc1f72de47ea387586436b29182f343dc
sillyangel <sillyangel3@gmail.com>
2022-04-30 16:59:32 -0500

    a

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const log = require("./modules/log.js")
+const client = require ("./modules/client.js")
 
 var verison = "1.0.0"
 var author = "@sillyangel"
 
-function Banana(string) {
-    if (string === "ver") {
-        log("is-Banana verison, " + verison + " and created by " + author);
-    }
-    if (string === "Help") {
-        log("Check out")
-    }
-}
 
-module.exports = Banana;
+
+
+
 
